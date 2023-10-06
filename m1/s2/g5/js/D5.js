562b53dacf826d9259c21e60ab672b289c7e78b4
fuza22 <barone.emanuele8@gmail.com>
2023-10-06 17:08:13 +0200

    test-s2

--- m1/s2/g5/js/D5.js
+++ m1/s2/g5/js/D5.js
@@ -186,6 +186,68 @@ for(i = 0; i < charactersArray.length; i++){
     case "a": 
       newArray.push(1)
       break;
+    case "b":
+      newArray.push(2)
+      break;
+    case "c":
+      newArray.push(3)
+      break;
+    case "d":
+      newArray.push(4)
+      break;
+    case "e":
+      newArray.push(5)
+      break;
+    case "f":
+      newArray.push(6)
+      break;
+    case "g":
+      newArray.push(7)
+      break;
+    case "h":
+      newArray.push(8)
+      break;
+    case "i":
+      newArray.push(9)
+      break;
+    case "l":
+      newArray.push(10)
+      break;
+    case "m":
+      newArray.push(11)
+      break;
+    case "n":
+      newArray.push(12)
+      break;
+    case "o":
+      newArray.push(13)
+      break;
+    case "p":
+      newArray.push(14)
+      break;
+    case "q":
+      newArray.push(15)
+      break;
+    case "r":
+      newArray.push(16)
+      break;
+    case "s":
+      newArray.push(17)
+      break;
+    case "t":
+      newArray.push(18)
+      break;
+    case "u":
+      newArray.push(19)
+      break;
+    case "v":
+      newArray.push(20)
+      break;
+    case "z":
+      newArray.push(21)
+      break;
+      
+
   }
 }
 console.log(newArray);
