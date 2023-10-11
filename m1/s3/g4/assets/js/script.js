afb107351bb4dd97d108e0764f76cacf219ab3ed
fuza22 <barone.emanuele8@gmail.com>
2023-10-11 17:04:48 +0200

    compito-s3-g3

---
+++ m1/s3/g4/assets/js/script.js
@@ -0,0 +1,45 @@
+let bottoneInvia = document.querySelector("#invia-form");
+
+
+bottoneInvia.addEventListener("click", function() {
+
+    let target = document.getElementById("lista");
+    let targetDone = document.getElementById("target-done")
+    let text = document.getElementById("testo");
+    let bottone = document.getElementById("invia-form")
+
+    if(!text.value) return;
+
+    let lista = document.createElement("li");
+    lista.classList.add("lista-elementi");
+    lista.textContent = text.value;
+    
+    lista.addEventListener("click", function(){
+
+        lista.classList.toggle("done");
+        
+    
+    })
+        target.append(lista);
+        text.value = '';
+
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-buttons")
+    deleteButton.innerText = "Cancella"
+
+    lista.appendChild(deleteButton)
+    deleteButton.addEventListener("click",function(){
+
+        lista.remove();
+        
+    })
+    
+
+
+    lista.addEventListener("click", function(){
+
+        
+
+    })
+
+})
