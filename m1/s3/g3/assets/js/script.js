bf33daaa33238eaeb62750546193400b829a4ce0
fuza22 <barone.emanuele8@gmail.com>
2023-10-12 17:06:54 +0200

    compito-s3-g4

---
+++ m1/s3/g3/assets/js/script.js
@@ -0,0 +1,20 @@
+let container = document.querySelector(".container");
+let bloccoNumeri = document.querySelector(".numeri-bingo");
+
+
+for(let i = 1; i <= 76; i++){
+
+  let bloccoNumero = document.createElement("div");
+  bloccoNumero.innerText = i;
+  bloccoNumeri.append(bloccoNumero);
+  
+}
+
+let bottone = querySelector(".bottone")
+
+bottone.addEventListener("click", function(){
+
+  let numeroEstratto = document.createElement("div");
+
+
+})
