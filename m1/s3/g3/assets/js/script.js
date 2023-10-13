7b3155ab2b2b5143bf2fb6d0ec4c3988fa2f90fa
fuza22 <barone.emanuele8@gmail.com>
2023-10-13 08:59:05 +0200

    modifiche-compito-s3-g4

--- m1/s3/g3/assets/js/script.js
+++ m1/s3/g3/assets/js/script.js
@@ -10,11 +10,18 @@ for(let i = 1; i <= 76; i++){
   
 }
 
-let bottone = querySelector(".bottone")
+let bottone = document.querySelector(".bottone");
+let boxNumeroEstratto = document.querySelector(".numero-estratto");
+
 
 bottone.addEventListener("click", function(){
 
-  let numeroEstratto = document.createElement("div");
+let numeroEstratto = Math.floor(Math.random() * 76) + 1;
+
+boxNumeroEstratto.innerText = numeroEstratto;
+
+bloccoNumeri.children[numeroEstratto - 1].style.backgroundColor = "red";
 
 
 })
+
