f4ea134ef3e36093afea3f06638b4a67e45c6384
fuza22 <barone.emanuele8@gmail.com>
2023-11-10 12:56:30 +0100

    test-s7

---
+++ m2/s3/g5/js/script.js
@@ -0,0 +1,52 @@
+const URL = "https://striveschool-api.herokuapp.com/api/product/"
+const APIKEY = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkMmU4MjI1NGU4ODAwMTgzZjE4MzkiLCJpYXQiOjE2OTk1NTY5OTQsImV4cCI6MTcwMDc2NjU5NH0.czdosu_eNwD-g1E25bBc4b3TbZKM5q7jpW5VPNi9C0c"
+
+
+async function getProducts(newProduct){
+
+
+let result = await fetch(`${URL}`,{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json',
+                    "Authorization": APIKEY
+                },
+                body: JSON.stringify(newProduct)
+            })
+
+}
+
+class Phone{
+
+    constructor(name, description, brand, imageUrl, price){
+
+        this.name = name;
+        this.description = description;
+        this.brand = brand;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+
+}
+
+let buttonAdd = document.querySelector(".buttonAdd");
+
+buttonAdd.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+
+    let name = document.querySelector("#validationCustom01").value;
+    let description = document.querySelector("#validationCustom02").value;
+    let brand = document.querySelector("#validationCustom03").value;
+    let imageUrl = document.querySelector("#validationCustom04").value;
+    let price = document.querySelector("#validationCustom05").value;
+
+
+    let newProduct = new Phone(name, description, brand, imageUrl, price);
+    console.log(newProduct);
+    getProducts(newProduct)
+
+});
+
