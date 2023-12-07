3f038f2943ee2cd4200963303a50bf7d9ba82265
fuza22 <barone.emanuele8@gmail.com>
2023-12-07 16:30:47 +0100

    first commit test

--- m3/s3/g4/meteoApp/src/app/app-routing.module.ts
+++ m3/s3/g4/meteoApp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) }, { path: 'login', loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
